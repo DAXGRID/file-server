b89f4bf480b0dd32e415f347be9a10bc87117f7f
runeanielsen <runenielsen@runbox.com>
2025-01-06 14:28:07 +0100

    configure http api project

---
+++ src/FileServer/Program.cs
@@ -0,0 +1,40 @@
+﻿using Serilog;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Formatting.Compact;
+
+internal static class Program
+{
+    public static async Task Main()
+    {
+        using var cancellationTokenSource = new CancellationTokenSource();
+        var cancellationToken = cancellationTokenSource.Token;
+
+        var builder = WebApplication.CreateBuilder();
+        builder.Logging.ClearProviders();
+        builder.Logging.AddSerilog(GetLogger(), true);
+        var app = builder.Build();
+
+        var loggerFactory = app.Services.GetService<ILoggerFactory>()
+            ?? throw new InvalidOperationException(
+                $"{nameof(ILoggerFactory)} is not configured in the IOC container.");
+
+        app.Logger.LogInformation("Starting the web service.");
+        var webServerTask = app.RunAsync(cancellationToken).ConfigureAwait(false);
+        app.Logger.LogInformation("The web service is now started.");
+
+        await webServerTask;
+
+        app.Logger.LogInformation("Shutting down...");
+    }
+
+    private static Logger GetLogger()
+    {
+        return new LoggerConfiguration()
+            .MinimumLevel.Override("System", LogEventLevel.Warning)
+            .MinimumLevel.Information()
+            .Enrich.FromLogContext()
+            .WriteTo.Console(new CompactJsonFormatter())
+            .CreateLogger();
+    }
+}
