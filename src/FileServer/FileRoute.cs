b772b8d8672f5b32d21f757897a90e40f21778fb
runeanielsen <runenielsen@runbox.com>
2025-01-06 17:00:27 +0100

    can now match on all routes

--- src/FileServer/FileRoute.cs
+++ src/FileServer/FileRoute.cs
@@ -4,14 +4,31 @@ internal static class FileRoute
 {
     public static void Setup(WebApplication app, ILoggerFactory loggerFactory)
     {
+        const string defaultPath = "";
         var logger = loggerFactory.CreateLogger(nameof(FileRoute));
 
         app.MapGet(
-            "/{*rest}",
-            (string route) =>
+            "{*route}",
+            (string? route) =>
             {
                 logger.LogInformation("Requested {FileName}...", route);
-                return Results.Ok(route);
+
+                if (route is null)
+                {
+                    return Results.Ok("/");
+                }
+
+                var filePath = Path.Combine(defaultPath, route);
+                var fileExists = File.Exists(filePath);
+
+                if (fileExists)
+                {
+                    return Results.Ok(route);
+                }
+                else
+                {
+                    return Results.NotFound();
+                }
             }
         );
     }
