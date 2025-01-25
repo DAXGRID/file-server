be421faa6a433e0da20227caa25b9605315b1fb2
runeanielsen <runenielsen@runbox.com>
2025-01-25 15:30:29 +0100

    switch back to sync writing to disk since we are blocking anyway

--- src/FileServer/FileRoute.cs
+++ src/FileServer/FileRoute.cs
@@ -86,7 +86,7 @@ internal static class FileRoute
 
         app.MapPost(
             "{*route}",
-            async (HttpContext context,
+            (HttpContext context,
              HttpRequest request,
              string route = "") =>
             {
@@ -131,8 +131,8 @@ internal static class FileRoute
                             using (FileStream outStream = new FileStream(tempFileName, FileMode.Create, FileAccess.Write, FileShare.None))
                             {
                                 uploadFileStream.Position = 0;
-                                await uploadFileStream.CopyToAsync(outStream).ConfigureAwait(false);
-                                await outStream.FlushAsync().ConfigureAwait(false); // Flush to OS
+                                uploadFileStream.CopyTo(outStream);
+                                outStream.Flush(true); // Flush to disk.
                             }
                         }
 
